a7888b41555805ddfb487e5162abc859d3f3933a
Alexey Deryugin <aderyugin@mirantis.com>
2015-12-30 16:06:03 +0300

    unit tests: make Keystone_endpoint match service by name/type
    Since a chance in puppet-keystone (1], we now match an endpoint with a
    service name/type.
    
    [1] http://git.openstack.org/cgit/openstack/puppet-keystone/commit/?id=0a4e06abb0f5b3f324464ff5219d2885816311ce
    
    Change-Id: I4287624264ff5f54d31f7eb1b89c25cdd502147f

--- spec/classes/murano_keystone_cfapi_auth_spec.rb
+++ spec/classes/murano_keystone_cfapi_auth_spec.rb
@@ -12,13 +12,12 @@ describe 'murano::keystone::cfapi_auth' do
         :tenant   => 'foobar' }
     end
 
-    it { is_expected.to contain_keystone_service('murano-cfapi').with(
+    it { is_expected.to contain_keystone_service('murano-cfapi::service_broker').with(
                             :ensure      => 'present',
-                            :type        => 'service_broker',
                             :description => 'Murano Service Broker API'
                         ) }
 
-    it { is_expected.to contain_keystone_endpoint('RegionOne/murano-cfapi').with(
+    it { is_expected.to contain_keystone_endpoint('RegionOne/murano-cfapi::service_broker').with(
                             :ensure       => 'present',
                             :public_url   => "http://127.0.0.1:8083",
                             :admin_url    => "http://127.0.0.1:8083",
@@ -34,7 +33,7 @@ describe 'murano::keystone::cfapi_auth' do
         :admin_url    => 'http://10.10.10.12:81' }
     end
 
-    it { is_expected.to contain_keystone_endpoint('RegionOne/murano-cfapi').with(
+    it { is_expected.to contain_keystone_endpoint('RegionOne/murano-cfapi::service_broker').with(
                             :ensure       => 'present',
                             :public_url   => 'https://10.10.10.10:80',
                             :internal_url => 'http://10.10.10.11:81',
@@ -48,7 +47,7 @@ describe 'murano::keystone::cfapi_auth' do
         :auth_name => 'muranoy' }
     end
 
-    it { is_expected.to contain_keystone_service('muranoy') }
-    it { is_expected.to contain_keystone_endpoint('RegionOne/muranoy') }
+    it { is_expected.to contain_keystone_service('muranoy::service_broker') }
+    it { is_expected.to contain_keystone_endpoint('RegionOne/muranoy::service_broker') }
   end
 end
